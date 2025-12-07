de20dd26c39bf1505e66e6a8eef552df08d430c8
Roberto Selbach <rselbach@rselbach.com>
2025-12-07 09:15:49 -0500

    Setup wsolver build

--- tools/wsolver/vite.config.js
+++ tools/wsolver/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/wsolver/',
   server: {
     port: 3000
   }
